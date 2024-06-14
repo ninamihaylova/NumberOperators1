5c7ca515289cbf26c73ef9ba7e5a1ee239ae87ed
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 23:52:06 +0300

    Initial commit

---
+++ NumberOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿class NumberOperations
+{
+    public static void Main(String[] args)
+    {
+        double num1 = double.Parse(Console.ReadLine());
+        double num2 = double.Parse(Console.ReadLine());
+        string mathOperator = Console.ReadLine();
+        double result = 0;
+
+        if (mathOperator == "+")
+        {
+            result = num1 + num2;
+            Console.WriteLine($"{num1} {mathOperator} {num2}= {result:F2} ");
+        }
+        else if (mathOperator == "-")
+        {
+            result = num1 - num2;
+            Console.WriteLine($" {num1} {mathOperator} {num2} = {result:F2}");
+        }
+        else if (mathOperator == "*")
+        {
+            result = num1 * num2;
+            Console.WriteLine($"{num1} {mathOperator} {num2}= {result:F2}");
+        }
+        else if (mathOperator == "/")
+        {
+            result = num1 / num2;
+            Console.WriteLine($"{num1} {mathOperator} {num2}= {result:F2}");
+        }
+    }
+}
